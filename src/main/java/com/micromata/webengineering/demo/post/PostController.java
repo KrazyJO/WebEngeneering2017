0d566ab2caabf1d5d855b2b259c73c35cbb50429
joern <joern@192.168.0.130>
2017-04-26 20:38:22 +0200

    implemented get/delete single post, switched back to list

--- src/main/java/com/micromata/webengineering/demo/post/PostController.java
+++ src/main/java/com/micromata/webengineering/demo/post/PostController.java
@@ -1,12 +1,13 @@
 package main.java.com.micromata.webengineering.demo.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+import java.util.UUID;
 
 /**
  * HTTP endpoint for a post-related HTTP requests.
@@ -20,9 +21,22 @@ public class PostController {
     public List<Post> getPostList() {
         return postService.getPosts();
     }
+    
+    @RequestMapping("/post/{guid}")
+    public Post getSinglePost(@PathVariable(value="guid") String guid)
+    {
+    	System.out.println("guid: " + guid);
+		return postService.getPostByGuid(java.util.UUID.fromString(guid));
+    }
 
     @RequestMapping(value = "/post/add", method = RequestMethod.POST)
     public void addPost(@RequestParam("title") String title) {
         postService.addPost(title);
+    }
+    
+    @RequestMapping(value = "/post/delete/{guid}", method = RequestMethod.DELETE)
+    public void deletePost(@PathVariable(value="guid") String guid)
+    {
+    	postService.deletePost(java.util.UUID.fromString(guid));
     }
 }
--- src/main/java/com/micromata/webengineering/demo/post/PostService.java
+++ src/main/java/com/micromata/webengineering/demo/post/PostService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Handle all CRUD operations for posts.
@@ -30,4 +31,35 @@ public class PostService {
     public void addPost(String title) {
         posts.add(new Post(title));
     }
+    
+    public Post getPostByGuid(UUID guid)
+    {
+    	//since there will be a database this should be good enough
+    	Post rValue = null;
+    	for (Post p : posts)
+    	{
+    		if (p.getGuid().equals(guid))
+    		{
+    			rValue = p;
+    			break;
+    		}
+    	}
+    	return rValue;
+    }
+
+    /**
+     * deletes a single post
+     * @param guid guid of the post
+     */
+	public void deletePost(UUID guid) {
+		// since there will be a database this should work good enough
+		for (Post p : posts)
+		{
+			if (p.getGuid().equals(guid))
+			{
+				posts.remove(p);
+				break;
+			}
+		}
+	}
 }
